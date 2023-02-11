0c243146b8f2bfa7327883b0dde32c833691ce6f
Stas Kotelnykov <staskotelnykov@gmail.com>
2023-02-11 07:39:02 +0200

    testing github actions

--- src/main/java/com/goit/feature/App.java
+++ src/main/java/com/goit/feature/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         SumCalculator sumCalculator = new SumCalculator();
         System.out.println("sumCalculator.sum(5) = " + sumCalculator.sum(5));
+        System.out.println("sumCalculator.sum(5) = " + sumCalculator.sum(3));
     }
 }
